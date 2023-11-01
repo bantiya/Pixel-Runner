32f1fb5f0a6b81294c138056000cc88f99774aa5
bantiya <bhavyaantiya@gmail.com>
2023-11-01 13:10:01 -0400

    Intro Screen

--- Code/09_transforming_surfaces/main.py
+++ Code/09_transforming_surfaces/main.py
@@ -2,12 +2,27 @@ import math
 import pygame
 from sys import exit  # # We use exit from sys to quit the programm in the if statement itself
 
+"""
+pygame.transform is used to do different things with the objects like:
+flip, scale, rotate, rotozoom, scale2x, smoothscale, ... , etc
+"""
+
 
 def display_score():
-    current_time = math.floor((pygame.time.get_ticks() - start_time)/1000)
+    current_time = math.floor((pygame.time.get_ticks() - start_time) / 1000)
     score_surf = test_font.render(str(current_time), False, (64, 64, 64))
-    score_rect = score_surf.get_rect(center = (400,50))
+    score_rect = score_surf.get_rect(center=(400, 50))
     screen.blit(score_surf, score_rect)
+    return current_time
+
+
+def display_final_score(prev_score, top_score):
+    final_score_surf = test_font.render("Your score: " + str(prev_score), False, (111,196,169))
+    final_score_rect = final_score_surf.get_rect(center=(400, 80))
+    top_score_surf = test_font.render("Top score: " + str(top_score), False, (111,196,169))
+    top_score_rect = final_score_surf.get_rect(topright=(800, 8))
+    screen.blit(final_score_surf, final_score_rect)
+    screen.blit(top_score_surf, top_score_rect)
 
 
 pygame.init()
@@ -24,7 +39,7 @@ clock = pygame.time.Clock()
 
 test_font = pygame.font.Font("../../font/Pixeltype.ttf", 50)
 
-game_active = True  # Maintain game state
+game_active = False  # Maintain game state
 start_time = 0
 
 #  ******************* Adding surfaces *******************
@@ -34,12 +49,27 @@ text_surface = test_font.render("My Game", False, (64, 64, 64)).convert_alpha()
 snail_surface = pygame.image.load("../../graphics/snail/snail1.png").convert_alpha()
 player_surface = pygame.image.load("../../graphics/Player/player_walk_1.png").convert_alpha()
 
+# Intro screen
+player_stand = pygame.image.load("../../graphics/Player/player_stand.png").convert_alpha()
+player_stand = pygame.transform.rotozoom(player_stand, 0, 2)
+game_title = test_font.render("Pixel Jumper", False, (111,196,169)).convert_alpha()
+instruction = test_font.render("Press Space to start the game", False, (111,196,169)).convert_alpha()
+
 player_gravity = 0  # Gravity variable for the player to jump
 
 #  ******************* Creating rectangle *******************
 player_rect = player_surface.get_rect(midbottom=(80, 300))
 snail_rect = snail_surface.get_rect(midbottom=(80, 300))
 score_rect = text_surface.get_rect(center=(400, 50))
+
+# Intro screen
+player_stand_rect = player_stand.get_rect(center=(400, 200))
+game_title_rect = game_title.get_rect(center=(400, 50))
+instruction_rect = instruction.get_rect(center=(400, 350))
+
+#  ******************* Calculating scores *******************
+top_score = 0
+final_score = 0
 
 while True:
     for event in pygame.event.get():
@@ -67,7 +97,8 @@ while True:
         #  ******************* Adding Frames *******************
         screen.blit(sky_surface, (0, 0))
         screen.blit(ground_surface, (0, 300))
-        display_score()
+        final_score = display_score()
+        if final_score > top_score: top_score = final_score
 
         # moving snail to left
         snail_rect.left -= 4
@@ -86,7 +117,11 @@ while True:
             game_active = False
 
     else:
-        screen.fill("Yellow")  # turn the game down if player is out
+        screen.fill((94, 129, 162))  # turn the game down if player is out
+        screen.blit(player_stand, player_stand_rect)
+        screen.blit(game_title, game_title_rect)
+        screen.blit(instruction, instruction_rect)
+        display_final_score(final_score, top_score)
 
     pygame.display.update()
     clock.tick(60)  # running at 60 fps
